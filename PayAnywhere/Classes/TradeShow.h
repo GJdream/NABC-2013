aee5147384417d73b83d2278185cb556d0e154eb
Jorge <jmontes530@gmail.com>
2013-03-28 16:46:51 -0400

    started work on tradeshow

--- PayAnywhere/Classes/TradeShow.h
+++ PayAnywhere/Classes/TradeShow.h
@@ -8,14 +8,19 @@
 
 #import <Foundation/Foundation.h>
 
-@interface TradeShow : NSObject{
-    }
+@interface TradeShow : NSObject
+{
+
+    NSMutableArray * applications;
+    NSMutableArray * agents;
+    
+}
+
 @property(strong, nonatomic) NSString *name;
 @property(strong, nonatomic) NSString *city;
 @property(strong, nonatomic) NSString *state;
 @property(strong, nonatomic) NSDate *date;
-@property(strong, nonatomic) NSMutableArray *agents;
-@property(strong, nonatomic) NSMutableArray *applications;
+
 
 
 @end
--- PayAnywhere/Classes/TradeShow.m
+++ PayAnywhere/Classes/TradeShow.m
@@ -10,4 +10,6 @@
 
 @implementation TradeShow
 
+
+
 @end
