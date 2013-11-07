ebfb5c08222b17ad80e82fa401aca682c09e9930
newuser <newuser@67-194-18-142.wireless.umnet.umich.edu>
2013-11-07 15:35:42 -0500

    Added Agent Admin interface

---
+++ PayAnywhere/AgentsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  AgentsTableViewController.h
+//  PayAnywhere
+//
+//  Created by newuser on 11/5/13.
+//  Copyright (c) 2013 NAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AgentsTableViewController : UITableViewController
+
+@end
