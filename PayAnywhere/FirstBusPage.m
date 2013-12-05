e0997067ad1f519a832d9e22fa9e37c1f56bf97b
newuser <newuser@67-194-44-112.wireless.umnet.umich.edu>
2013-12-05 03:09:45 -0500

    active agent

--- PayAnywhere/FirstBusPage.m
+++ PayAnywhere/FirstBusPage.m
@@ -142,11 +142,19 @@ bool fieldsOn;
 -(void)viewWillAppear:(BOOL)animated
 {
     MarketSource *activeTradeshow = [[Database sharedDB] getActiveTradeshow];
+    Agent *activeAgent = [[Database sharedDB] getActiveAgent];
     if (activeTradeshow != nil) {
         self.activeTradeshowLabel.text = activeTradeshow.name;
     }
     else {
         self.activeTradeshowLabel.text = @"";
+    }
+    
+    if (activeAgent != nil) {
+        self.currentAgentLabel.text = [NSString stringWithFormat:@"%@ %@", activeAgent.firstName, activeAgent.lastName];
+    }
+    else {
+        self.currentAgentLabel.text = @"";
     }
 }
 
